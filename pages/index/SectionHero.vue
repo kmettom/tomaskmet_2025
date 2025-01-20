44e9d7b83f283aacaf6859f2f68bf3c6dbcca037
kmettom <kmettom@gmail.com>
2025-01-20 09:45:55 +0000

    Hero

--- pages/index/SectionHero.vue
+++ pages/index/SectionHero.vue
@@ -12,7 +12,7 @@
           </CanvasText>
         </h2>
       </div>
-      <div class="hero-content-line">
+      <div class="hero-content-line hero-line-783">
         <div class="body-m hero-content-sm hero-services">
           <p>web3</p>
           <p>creative</p>
@@ -59,24 +59,27 @@ import Container from '~/components/common/Container.vue';
   justify-content: center;
   &.hero-line-studio {
     display: block;
-    margin-top: 25px;
-    //margin-bottom: -15px;
+    margin: -55px 0px -135px 0;
   }
+  &.hero-line-783{
+    //height: 350px;
+  }
+
 }
 
 .hero-content-sm {
   position: absolute;
-  padding: 0px 10px;
+  padding: 0px 20px;
   width: 300px;
   &.hero-services {
     text-align: right;
     left: -300px;
-    bottom: 30px;
+    bottom: 100px;
   }
   &.hero-summary {
     text-align: left;
     right: -300px;
-    top: 0;
+    top: 50px;
   }
 }
 </style>
