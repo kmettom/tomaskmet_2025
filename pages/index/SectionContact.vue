f3f7ef743f67a9d392f30679aeb811fa6079d79d
kmettom <kmettom@gmail.com>
2025-01-22 16:44:37 +0000

    Contact

--- pages/index/SectionContact.vue
+++ pages/index/SectionContact.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="callToActionSection">
+  <div class="contact-section">
     <Container>
       <div class="frame">
         <div class="heading-1">
@@ -9,7 +9,7 @@
               :shader="'default'"
               :theme="'dark'"
             >
-              Get
+              GET
             </CanvasText>
             <span v-scrollActive="0.85" class="canvas-text-spacing">
               <CanvasText
@@ -17,12 +17,12 @@
                 :shader="'default'"
                 :theme="'dark'"
               >
-                in
+                IN
               </CanvasText>
             </span>
           </span>
         </div>
-        <div id="secondPart">
+        <div class="contact-line second-line">
           <div class="heading-1">
             <span v-scrollActive="0.85">
               <CanvasText
@@ -30,35 +30,39 @@
                 :shader="'default'"
                 :theme="'dark'"
               >
-                touch
+                TOUCH
               </CanvasText>
             </span>
           </div>
-          <div id="actionList" class="body-m">
+          <div class="action-list body-m">
             <div>
-              <!--               <div class="iconFrame">-->
-              <!--                <img :src="PhoneIcon" alt="Phone icon">-->
-              <!--              </div> -->
               <span>📞 Book a call</span>
             </div>
             <div>
               <!-- <div class="iconFrame">
                 <img :src="LetterIcon" alt="Letter icon">
               </div> -->
-              <a href="mailto:hello@783studio.com">
+              <a
+                href="mailto:hello@783studio.com"
+                data-email="hello@783studio.com"
+              >
                 <span>✉️ Send a request</span>
               </a>
             </div>
           </div>
-          <div id="socialMediaList" class="body-m">
+          <div class="social-media-list body-m">
             <div>
-              <a href="https://www.instagram.com/kmettom/" target="_blank"
+              <a href="https://www.instagram.com/783.studio" target="_blank"
                 >Instagram</a
               >
             </div>
-            <div><a href="" target="_blank">Email</a></div>
+            <!--            <div><a href="" target="_blank">Email</a></div>-->
             <div>
-              <a href="https://twitter.com/KmetTom" target="_blank">Twitter</a>
+              <a
+                href="mailto:hello@783studio.com"
+                data-email="hello@783studio.com"
+                >Email</a
+              >
             </div>
             <div>
               <a href="https://www.linkedin.com/in/tomas-kmet/" target="_blank"
@@ -84,112 +88,13 @@ import BasketBallIcon from '~/public/icons/basket-ball.png';
 
 <style lang="scss" scoped>
 //=======>>>   CTA   <<<==========//
-#callToActionSection {
+.contact-section {
   position: relative;
   color: var(--dark-color);
   background-color: var(--light-color);
 
   padding-top: 150px;
   padding-bottom: 150px;
-
-  .frame {
-    display: flex;
-    flex-direction: column;
-
-    @include respond-width($w-m) {
-      margin: 0 auto;
-      max-width: 598px;
-      height: 420px;
-    }
-
-    @include respond-width($w-s) {
-      max-width: 300px;
-      height: 188px;
-    }
-  }
-
-  #secondPart {
-    position: relative;
-    align-self: flex-end;
-    bottom: 40px;
-
-    @include respond-width($w-m) {
-      bottom: 24px;
-    }
-
-    @include respond-width($w-s) {
-      bottom: 10px;
-    }
-
-    #actionList {
-      position: absolute;
-      right: 0;
-      bottom: 100%;
-      padding-right: 240px;
-      padding-bottom: 18px;
-
-      @include respond-width($w-m) {
-        padding-right: 0px;
-        padding-bottom: 14px;
-      }
-
-      @include respond-width($w-s) {
-        right: calc(100% + 4px);
-        width: 100px;
-        bottom: 0;
-        height: 100%;
-        padding: 0;
-
-        // first option
-        & > div:first-child {
-          margin-bottom: 15px;
-
-          // first option icon
-          .iconFrame {
-            position: relative;
-            left: 10px;
-          }
-        }
-      }
-    }
-
-    #socialMediaList {
-      position: absolute;
-      text-align: right;
-      bottom: 50px;
-      right: 100%;
-      padding-bottom: 5px;
-      padding-right: 10px;
-
-      @include respond-width($w-m) {
-        bottom: 36px;
-        padding-bottom: 0px;
-        padding-right: 10px;
-      }
-
-      @include respond-width($w-s) {
-        text-align: left;
-        left: 5px;
-        top: calc(100% - 10px);
-      }
-    }
-  }
-
-  // single action item
-  & #actionList > div {
-    display: flex;
-
-    @include respond-width($w-s) {
-      text-align: right;
-    }
-
-    & .iconFrame {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      margin-right: 4px;
-    }
-  }
 
   & #bottomElement {
     width: 150px;
@@ -220,6 +125,83 @@ import BasketBallIcon from '~/public/icons/basket-ball.png';
     padding-left: 20px;
     position: relative;
     display: inline-block;
+  }
+}
+.frame {
+  display: flex;
+  flex-direction: column;
+
+  @include respond-width($w-m) {
+    margin: 0 auto;
+    max-width: 598px;
+    height: 420px;
+  }
+
+  @include respond-width($w-s) {
+    max-width: 300px;
+    height: 188px;
+  }
+}
+
+.contact-line.second-line {
+  position: relative;
+}
+
+.social-media-list {
+  position: absolute;
+  text-align: right;
+  bottom: 50px;
+  right: 100%;
+  padding-bottom: 5px;
+  padding-right: 10px;
+
+  @include respond-width($w-m) {
+    bottom: 36px;
+    padding-bottom: 0px;
+    padding-right: 10px;
+  }
+
+  @include respond-width($w-s) {
+    text-align: left;
+    left: 5px;
+    top: calc(100% - 10px);
+  }
+}
+
+.action-list {
+  position: absolute;
+  right: 0;
+  bottom: 100%;
+  padding-right: 240px;
+  padding-bottom: 18px;
+
+  @include respond-width($w-m) {
+    padding-right: 0px;
+    padding-bottom: 14px;
+  }
+
+  @include respond-width($w-s) {
+    right: calc(100% + 4px);
+    width: 100px;
+    bottom: 0;
+    height: 100%;
+    padding: 0;
+  }
+
+  // single action item
+  //&  > div {
+  //  display: flex;
+  //  @include respond-width($w-s) {
+  //    text-align: right;
+  //  }
+  //
+  //}
+
+  .iconFrame {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 4px;
   }
 }
 </style>
