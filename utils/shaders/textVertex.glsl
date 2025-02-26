3aaf43deca3e100f4e6b6f219ce252848f94b062
kmettom <kmettom@gmail.com>
2025-02-26 17:41:43 +0000

    Project Image shader

--- utils/shaders/textVertex.glsl
+++ utils/shaders/textVertex.glsl
@@ -7,18 +7,18 @@ varying vec2 vUv;
 
 void main() {
   float distVnoise = distance(uv, vectorVNoise);
-  vNoise = 2.0 * hoverState * sin(distVnoise * 10.0 - time);
+//  vNoise = 2.0 * hoverState * sin(distVnoise * 10.0 - time);
 
   float distWave = distance(uv, vectorVNoise);
 
   vec3 newposition = position;
 
-  newposition.z += (1.0 - aniIn) * 70.0 * sin(distWave * 333.0 + time);
+//  newposition.z += (1.0 - aniIn) * 70.0 * sin(distWave * 333.0 + time);
 
   vUv = uv;
   gl_Position =
     projectionMatrix *
     modelViewMatrix *
-    vec4(newposition, 1.0 + 0.0 * (1.0 - aniIn) - hoverState / 20.0);
+    vec4(newposition, 1.0);
 
 }
