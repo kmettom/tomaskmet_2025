058c13c1c90f8777d45436b59a57df6a562439f0
kmettom <kmettom@gmail.com>
2025-02-27 18:56:03 +0000

    Project shaders

--- utils/shaders/projectFragment.glsl
+++ utils/shaders/projectFragment.glsl
@@ -56,7 +56,7 @@ vec4 transition(vec2 uv) {
 
   float smoothness = 0.01;
 
-  float p = mix(-smoothness, 1.0 + smoothness, uAniIn - uHover );
+  float p = mix(-smoothness, 1.0 + smoothness, uAniIn - uHover);
   //  float p = mix(-smoothness, 1.0 + smoothness, 1.0 - uHover);
   float lower = p - smoothness;
   float higher = p + smoothness;
