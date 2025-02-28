d5426c5e503e2bcf8d9bb804fb6b6c0bea2fad5a
kmettom <kmettom@gmail.com>
2025-02-28 13:15:09 +0000

    Project shaders

--- utils/shaders/projectFragment.glsl
+++ utils/shaders/projectFragment.glsl
@@ -15,63 +15,79 @@ uniform float seed; // = 12.9898
 uniform vec2 uMeshSize; // The size of the mesh (width, height)
 uniform vec2 uTextureSize; // The size of the texture (width, height)
 
-float random(vec2 co) {
-  highp float a = 12.9;
-  highp float b = 78.233;
-  highp float c = 43758.5453;
-  highp float dt = dot(co.xy, vec2(a, b));
-  highp float sn = mod(dt, 3.14);
-  return fract(sin(sn) * c);
+// Hash function for 2D coordinates
+float hash(vec2 p) {
+  return fract(sin(dot(p, vec2(127.1, 311.7))) * 43758.5453123);
 }
 
-// 2D Noise based on Morgan McGuire @morgan3d
-// https://www.shadertoy.com/view/4dS3Wd
-float noise(vec2 st) {
-  vec2 i = floor(st);
-  vec2 f = fract(st);
-
-  // Four corners in 2D of a tile
-  float a = random(i);
-  float b = random(i + vec2(1.0, 0.0));
-  float c = random(i + vec2(0.0, 1.0));
-  float d = random(i + vec2(1.0, 1.0));
-
-  // Smooth Interpolation
-
-  // Cubic Hermine Curve.  Same as SmoothStep()
+// 2D Noise function
+float noise(vec2 p) {
+  vec2 i = floor(p);
+  vec2 f = fract(p);
+  float a = hash(i);
+  float b = hash(i + vec2(1.0, 0.0));
+  float c = hash(i + vec2(0.0, 1.0));
+  float d = hash(i + vec2(1.0, 1.0));
   vec2 u = f * f * (3.0 - 2.0 * f);
-  u = smoothstep(0.0, 1.0, f);
-
-  // Mix 4 coorners porcentages
   return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
 }
 
-vec4 transition(vec2 uv) {
-  vec4 from =
-    texture2D(uImage, uv) *
-    vec4(1.0, 1.0, 1.0, min(uAniIn + uImageGallery, 1.0));
-  vec4 to = texture2D(uImage, uv) * vec4(1.0, 1.0, 1.0, 1.0);
-  float n = noise(uv * 4.0);
-
-  float smoothness = 0.01;
-
-  float p = mix(-smoothness, 1.0 + smoothness, uAniIn - uHover);
-  //  float p = mix(-smoothness, 1.0 + smoothness, 1.0 - uHover);
-  float lower = p - smoothness;
-  float higher = p + smoothness;
-
-  float q = smoothstep(lower, higher, n);
-
-  return mix(from, to, 1.0 - q);
+// Fractal Brownian Motion for rich noise detail
+float fbm(vec2 p) {
+  float f = 0.0;
+  float w = 0.5;
+  for (int i = 0; i < 5; i++) {
+    f += w * noise(p);
+    p *= 2.0;
+    w *= 0.5;
+  }
+  return f;
 }
 
 //void main() {
-//  gl_FragColor = transition(vUv);
-//  gl_FragColor.rgb += 0.01 * vec3(vNoise);
+//  // Normalize coordinates to [-1,1] and preserve aspect ratio
+////  vec2 uv = gl_fragCoord.xy / uMeshSize.xy * 2.0 - 1.0;
+////  uv.x *= uMeshSize.x / uMeshSize.y;
+
+
+//  vec2 uv = vUv;
 //
+//  // Time factor for animation
+//  float t = time * 0.2;
+//
+//  // Convert to polar coordinates
+//  float angle = atan(uv.y, uv.x);
+//  float radius = length(uv);
+//
+//  // Apply a swirling distortion using fractal noise
+//  angle += fbm(uv * 3.0 + t);
+//  vec2 swirled = vec2(cos(angle), sin(angle)) * radius;
+//
+//  // Compute fractal noise pattern for the nebula texture
+//  float n = fbm(swirled * 2.0 + vec2(t, t));
+//
+//  // Create a blooming nebula effect using smooth thresholding
+//  float nebula = smoothstep(0.3, 0.6, n);
+//
+//  // Define a rich, cosmic color palette
+//  vec3 col1 = vec3(0.1, 0.0, 0.3); // deep cosmic purple
+//  vec3 col2 = vec3(0.8, 0.3, 0.9); // vibrant magenta
+//  vec3 col3 = vec3(0.0, 0.7, 0.9); // cool turquoise
+//
+//  // Blend colors based on the noise value
+//  vec3 color = mix(col1, col2, n);
+//  color = mix(color, col3, smoothstep(0.2, 0.4, n));
+//
+//  // Enhance vibrancy with gamma correction
+//  color = pow(color, vec3(1.5));
+//
+//  // Final color output modulated by the nebula bloom effect
+//  gl_fragColor = vec4(color * nebula, 1.0);
+////  gl_fragColor = vUv;
 //}
 
 void main() {
+
   // Calculate the aspect ratios
   float meshAspect = uMeshSize.x / uMeshSize.y;
   float textureAspect = uTextureSize.x / uTextureSize.y;
@@ -88,22 +104,36 @@ void main() {
     uv.x = uv.x * scale + (1.0 - scale) / 2.0; // Center the texture horizontally
   }
 
-  //  vec2 newUV = vUv;
-  //
-  //  vec2 p = newUV;
-  //  float x = uHover + 1.0 - uAniIn;
-  //  x = smoothstep(0.0, 1.0, x * 2.0 + p.y - 1.0);
-  //  vec4 f = mix(
-  //    texture2D(uImage, (p - 0.5) * (1.0 - x) + 0.5),
-  //    texture2D(uImage, (p - 0.5) * x + 0.5),
-  //    x
-  //  );
-  //
-  //  gl_FragColor = f * vec4(1.0, 1.0, 1.0, uAniIn);
-  //  gl_FragColor.rgb += 0.05 * vec3(vNoise);
+    float t = time * 0.12;
+    // Convert to polar coordinates
+    float angle = atan(uv.y, uv.x);
+    float radius = length(uv);
+    // Apply a swirling distortion using fractal noise
+    angle += fbm(uv * 3.0 + t);
+    vec2 swirled = vec2(cos(angle), sin(angle)) * radius;
+    // Compute fractal noise pattern for the nebula texture
+  // Sample the texture using the provided texture coordinates
+  vec4 texColor = texture2D(uImage, swirled);
 
-  gl_FragColor = transition(uv);
-  //  gl_FragColor = f * vec4(1.0, 1.0, 1.0, uAniIn);
-  gl_FragColor.rgb += 0.01 * vec3(vNoise);
 
+//  float n = fbm(swirled * 2.0 + vec2(t, t));
+//  float nebula = smoothstep(0.3, 0.6, n);
+//    // Define a rich, cosmic color palette
+//    vec3 col1 = vec3(0.1, 0.0, 0.3); // deep cosmic purple
+//    vec3 col2 = vec3(0.8, 0.3, 0.9); // vibrant magenta
+//    vec3 col3 = vec3(0.0, 0.7, 0.9); // cool turquoise
+//
+//    // Blend colors based on the noise value
+//    vec4 color = mix(col1, col2, col3, n);
+//    color = mix(color, col3, smoothstep(0.2, 0.4, n));
+
+    // Enhance vibrancy with gamma correction
+//    color = pow(color, vec3(1.5));
+//  vec4 combinedColor = mix(texColor, nebula, 0.5);
+
+
+  // Output the final color
+//    gl_fragColor = vec4(color * nebula, 1.0);
+  gl_FragColor = texColor;
+//  gl_FragColor = combinedColor;
 }
---
+++ utils/shaders/projectFragmentB.glsl
@@ -0,0 +1,95 @@
+varying float vNoise;
+varying vec2 vUv;
+uniform sampler2D uImage;
+
+uniform float time;
+uniform float uHover;
+uniform float uAniIn;
+uniform float uImageGallery;
+uniform float uImageGalleryActive;
+
+uniform float scale; // = 4.0
+uniform float smoothness; // = 0.01
+uniform float seed; // = 12.9898
+
+uniform vec2 uMeshSize; // The size of the mesh (width, height)
+uniform vec2 uTextureSize; // The size of the texture (width, height)
+
+float random(vec2 co) {
+  highp float a = 12.9;
+  highp float b = 78.233;
+  highp float c = 43758.5453;
+  highp float dt = dot(co.xy, vec2(a, b));
+  highp float sn = mod(dt, 3.14);
+  return fract(sin(sn) * c);
+}
+
+// 2D Noise based on Morgan McGuire @morgan3d
+// https://www.shadertoy.com/view/4dS3Wd
+float noise(vec2 st) {
+  vec2 i = floor(st);
+  vec2 f = fract(st);
+
+  // Four corners in 2D of a tile
+  float a = random(i);
+  float b = random(i + vec2(1.0, 0.0));
+  float c = random(i + vec2(0.0, 1.0));
+  float d = random(i + vec2(1.0, 1.0));
+
+  // Smooth Interpolation
+
+  // Cubic Hermine Curve.  Same as SmoothStep()
+  vec2 u = f * f * (3.0 - 2.0 * f);
+  u = smoothstep(0.0, 1.0, f);
+
+  // Mix 4 coorners porcentages
+  return mix(a, b, u.x) + (c - a) * u.y * (1.0 - u.x) + (d - b) * u.x * u.y;
+}
+
+vec4 transition(vec2 uv) {
+  vec4 from =
+    texture2D(uImage, uv) *
+    vec4(1.0, 1.0, 1.0, min(uAniIn + uImageGallery, 1.0));
+  vec4 to = texture2D(uImage, uv) * vec4(1.0, 1.0, 1.0, 1.0);
+  float n = noise(uv * 4.0);
+
+  float smoothness = 0.01;
+
+  float p = mix(-smoothness, 1.0 + smoothness, uAniIn - uHover);
+  //  float p = mix(-smoothness, 1.0 + smoothness, 1.0 - uHover);
+  float lower = p - smoothness;
+  float higher = p + smoothness;
+
+  float q = smoothstep(lower, higher, n);
+
+  return mix(from, to, 1.0 - q);
+}
+
+//void main() {
+//  gl_FragColor = transition(vUv);
+//  gl_FragColor.rgb += 0.01 * vec3(vNoise);
+//
+//}
+
+void main() {
+  // Calculate the aspect ratios
+  float meshAspect = uMeshSize.x / uMeshSize.y;
+  float textureAspect = uTextureSize.x / uTextureSize.y;
+
+  // Adjust UV coordinates to maintain aspect ratio
+  vec2 uv = vUv;
+  if (meshAspect > textureAspect) {
+    // Mesh is wider than the texture
+    float scale = textureAspect / meshAspect;
+    uv.y = uv.y * scale + (1.0 - scale) / 2.0; // Center the texture vertically
+  } else {
+    // Mesh is taller than the texture
+    float scale = meshAspect / textureAspect;
+    uv.x = uv.x * scale + (1.0 - scale) / 2.0; // Center the texture horizontally
+  }
+
+  gl_FragColor = transition(uv);
+  //  gl_FragColor = f * vec4(1.0, 1.0, 1.0, uAniIn);
+  gl_FragColor.rgb += 0.01 * vec3(vNoise);
+
+}
