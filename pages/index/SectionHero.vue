5bee1131a47662f955d0961990865cbd51e9cb09
kmettom <kmettom@gmail.com>
2025-03-14 10:53:03 +0000

    Hero webp

--- pages/index/SectionHero.vue
+++ pages/index/SectionHero.vue
@@ -32,7 +32,7 @@
     </Container>
     <div class="hero-bg-image">
       <CanvasImage
-        :src-link="'images/hero.png'"
+        :src-link="'images/hero.webp'"
         :uniforms="imageUniforms"
         :shader="'hero'"
         :load-strategy="'preload'"
