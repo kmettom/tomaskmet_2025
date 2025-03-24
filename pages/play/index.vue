42e6f5af13e791e43552cfc906c367edee65033e
kmettom <kmettom@gmail.com>
2025-03-24 17:10:25 +0000

    Playground site update

---
+++ pages/play/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="page-container">
+    <h1 class="headline">
+      <CanvasText :theme="'light'"> Playground </CanvasText>
+    </h1>
+  </div>
+</template>
+<script setup>
+import { onMounted, watch } from 'vue';
+
+useSeoMeta({
+  title: 'Tomas Kmet - Web developer - Playground',
+  ogTitle: 'Tomas Kmet - Web developer - Playground',
+  description: 'Tomas Kmet - Web developer - Playground',
+  ogDescription: 'Tomas Kmet - Web developer - Playground',
+});
+
+const props = defineProps({
+  pageActive: Boolean,
+});
+
+const hoverImage = ref(false);
+// const textHover = ref(false);
+
+const pageAnimateIn = () => {
+  // console.log('Example method - animate page transition');
+};
+
+onMounted(() => {
+  if (props.pageActive) {
+    pageAnimateIn();
+  }
+});
+
+watch(
+  () => props.pageActive,
+  (newValue) => {
+    if (newValue) {
+      pageAnimateIn();
+    }
+  },
+);
+</script>
+<style lang="scss" scoped></style>
