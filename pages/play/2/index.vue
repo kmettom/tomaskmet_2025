b8008ac063e16b03ebd08b2bc384ccf76dbecd93
kmettom <kmettom@gmail.com>
2025-04-28 21:23:11 +0800

    Play 2 - infinite scroll begin

---
+++ pages/play/2/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="page-container">
+    <h1 class="headline">
+      <CanvasText :theme="'light'" :uniforms="mainTextInUniforms">
+        1
+      </CanvasText>
+    </h1>
+    <div ref="play2el" class="play2-el" />
+  </div>
+</template>
+<script setup>
+import { onMounted } from 'vue';
+import { infiniteLoopScrollSection } from '~/utils/play/play2';
+
+useSeoMeta({
+  title: 'Tomas Kmet - Web developer - Playground',
+  ogTitle: 'Tomas Kmet - Web developer - Playground',
+  description: 'Tomas Kmet - Web developer - Playground',
+  ogDescription: 'Tomas Kmet - Web developer - Playground',
+});
+
+const mainTextInUniforms = {
+  uAniInText: { value: 1, duration: 1 },
+};
+
+const play2el = ref('play2el');
+
+onMounted(() => {
+  setTimeout(() => {
+    infiniteLoopScrollSection(play2el.value);
+  }, 1000);
+});
+</script>
+<style lang="scss" scoped>
+.play2-el {
+  width: 100%;
+}
+</style>
