f2234637906fa3553ce09726ff7a60e43ec60048
kmettom <kmettom@gmail.com>
2025-06-06 20:11:53 +0700

    :construction: Play 1 - image on sphere

--- utils/shaders/play1Fragment.glsl
+++ utils/shaders/play1Fragment.glsl
@@ -8,16 +8,35 @@ uniform float uAniIn;
 uniform float uImageGallery;
 uniform float uImageGalleryActive;
 
-uniform float scale; // = 4.0
-uniform float smoothness; // = 0.01
-uniform float seed; // = 12.9898
+uniform float scale;// = 4.0
+uniform float smoothness;// = 0.01
+uniform float seed;// = 12.9898
 
-uniform vec2 uMeshSize; // The size of the mesh (width, height)
-uniform vec2 uTextureSize; // The size of the texture (width, height)
+uniform vec2 uMeshSize;// The size of the mesh (width, height)
+uniform vec2 uTextureSize;// The size of the texture (width, height)
 
 void main() {
-  gl_FragColor = texture2D(uImage, vUv);
 
-  //  gl_FragColor = vec4(vUv, 1.0, 1.0);
 
+    float meshAspect = uMeshSize.x / uMeshSize.y;
+    float textureAspect = uTextureSize.x / uTextureSize.y;
+    //
+    // Adjust UV coordinates to maintain aspect ratio
+    vec2 uv = vUv;
+    if (meshAspect > textureAspect) {
+        // Mesh is wider than the texture
+        float scale = textureAspect / meshAspect;
+        uv.y = uv.y * scale + (1.0 - scale) / 2.0;// Center the texture vertically
+    } else {
+        // Mesh is taller than the texture
+        float scale = meshAspect / textureAspect;
+        uv.x = uv.x * scale + (1.0 - scale) / 2.0;// Center the texture horizontally
+    }
+    //
+    //  gl_FragColor = texture2D(uImage, vUv);
+
+    //  vec2 uv = vUv;
+    //  uv.x = fract(uv.x + uTime * 0.05); // wrap horizontally
+    //  if (uv.x < 0.25 || uv.x > 0.75) discard;
+    gl_FragColor = texture2D(uImage, uv);
 }
--- utils/shaders/play1Vertex.glsl
+++ utils/shaders/play1Vertex.glsl
@@ -5,8 +5,11 @@ varying float vNoise;
 varying vec2 vUv;
 
 void main() {
+  vUv = uv; // Capture UV
   vec3 newposition = position;
-  vUv = uv;
   gl_Position = projectionMatrix * modelViewMatrix * vec4(newposition, 1.0);
+
+//  vUv = uv + vec2(uTime * 0.05, 0.0); // adjust speed
+//  gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
 
