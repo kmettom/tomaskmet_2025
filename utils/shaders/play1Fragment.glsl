c6a0c7ff7b6d7ad1e36419f6a04601835b8c4432
kmettom <kmettom@gmail.com>
2025-06-07 22:54:26 +0700

    :construction: Play 1 - image on sphere

--- utils/shaders/play1Fragment.glsl
+++ utils/shaders/play1Fragment.glsl
@@ -8,16 +8,16 @@ uniform float uAniIn;
 uniform float uImageGallery;
 uniform float uImageGalleryActive;
 
-uniform float scale;// = 4.0
-uniform float smoothness;// = 0.01
-uniform float seed;// = 12.9898
+uniform float scale; // = 4.0
+uniform float smoothness; // = 0.01
+uniform float seed; // = 12.9898
 
-uniform vec2 uMeshSize;// The size of the mesh (width, height)
-uniform vec2 uTextureSize;// The size of the texture (width, height)
+uniform vec2 uMeshSize; // The size of the mesh (width, height)
+uniform vec2 uTextureSize; // The size of the texture (width, height)
 
 void main() {
-    vec2 uv = vUv;
-    uv.x = fract(uv.x + uTime * 0.05); // wrap horizontally
-    uv.x = fract(uv.x + uTime * 0.05);
-    gl_FragColor = texture2D(uImage, uv);
+  vec2 uv = vUv;
+  uv.x = fract(uv.x + uTime * 0.05); // wrap horizontally
+  uv.x = fract(uv.x + uTime * 0.05);
+  gl_FragColor = texture2D(uImage, uv);
 }
--- utils/shaders/play1Vertex.glsl
+++ utils/shaders/play1Vertex.glsl
@@ -7,7 +7,7 @@ varying vec2 vUv;
 void main() {
   vUv = uv; // Capture UV
 
-//  vUv = uv + vec2(uTime * 0.05, 0.0); // adjust speed
+  //  vUv = uv + vec2(uTime * 0.05, 0.0); // adjust speed
   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
 
